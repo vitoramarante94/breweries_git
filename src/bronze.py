89bb6ca2c7d0c1c8f9e58d690666c033f12026de
vitoramarante94 <vitoramarante.94@gmail.com>
2026-04-01 20:47:59 -0300

    Ajuste de request para retonar todas as paginas de resultados

--- src/bronze.py
+++ src/bronze.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import requests
+from itertools import count
 
 def ingestao_bronze():
-    # Buscar dados da API
-    response = requests.get("https://api.openbrewerydb.org/breweries")
-    data = response.json()
-    print(data)
+    # Buscar dados da API com paginacao
+    per_page = 100
+    data = []
+
+    for page in count(1):
+        params = {"page": page, "per_page": per_page}
+        response = requests.get("https://api.openbrewerydb.org/breweries", params=params)
+        response.raise_for_status()
+        page_data = response.json()
+
+        if not page_data:
+            break
+
+        data.extend(page_data)
+
     # Converter a lista de dados em um DataFrame do Pandas
     df = pd.DataFrame(data)
 
